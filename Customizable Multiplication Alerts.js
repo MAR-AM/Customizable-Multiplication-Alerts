6e36d5b0ff78e16d6ed02bfea958b07ae7edfbc0
MARIEM-AMZIL <147107786+MAR-AM@users.noreply.github.com>
2024-04-27 15:00:21 +0100

    Customizable Multiplication Alerts.js

---
+++ Customizable Multiplication Alerts.js
@@ -0,0 +1,11 @@
+var a = 3
+    var b = 2
+
+    function multiple(x=8){
+        return x*3
+    }
+    function affichage(){
+        alert(multiple(a))
+        alert(multiple(b))
+        alert(multiple())
+    }
